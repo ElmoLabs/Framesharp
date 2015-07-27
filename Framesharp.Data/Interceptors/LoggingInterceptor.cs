10ea3db44c12f334f8f77c744533380781a1432a
claudiocidade <claudiocidade@hotmail.com>
2015-07-27 08:51:26 -0300

    Clean up

--- Framesharp.Data/Interceptors/LoggingInterceptor.cs
+++ Framesharp.Data/Interceptors/LoggingInterceptor.cs
@@ -5,7 +5,7 @@ using NHibernate.SqlCommand;
 
 namespace Framesharp.Data.Interceptors
 {
-    public class LoggingInterceptor : EmptyInterceptor, IInterceptor
+    public class LoggingInterceptor : EmptyInterceptor
     {
         public override SqlString OnPrepareStatement(SqlString sql)
         {
